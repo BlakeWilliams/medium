fd2be85d50d5efe6a93573cd77c33fee7e6a0700
Blake Williams <blake@blakewilliams.me>
2023-07-01 12:41:36 -0400

    Make apptest more generic, focus on sessions/responses

--- pkg/apptest/apptest.go
+++ pkg/apptest/apptest.go
@@ -7,8 +7,6 @@ import (
 	"net/http/httptest"
 	"net/url"
 	"strings"
-
-	"github.com/blakewilliams/medium"
 )
 
 var RedirectCodes = []int{
@@ -20,14 +18,13 @@ var RedirectCodes = []int{
 }
 
 // Represents a request, or requests to a given medium application.
-type AppRequest[T medium.Action] struct {
-	CookieJar    http.CookieJar
-	LastResponse *httptest.ResponseRecorder
-	medium       *medium.Router[T]
+type Session struct {
+	CookieJar http.CookieJar
+	medium    http.Handler
 }
 
-func New[T medium.Action](medium *medium.Router[T]) *AppRequest[T] {
-	req := &AppRequest[T]{}
+func New(medium http.Handler) *Session {
+	req := &Session{}
 	jar, err := cookiejar.New(nil)
 	if err != nil {
 		panic(err)
@@ -38,11 +35,11 @@ func New[T medium.Action](medium *medium.Router[T]) *AppRequest[T] {
 	return req
 }
 
-func (ar *AppRequest[T]) Get(route string, headers http.Header) {
-	ar.makeRequest(http.MethodGet, route, headers, nil)
+func (ar *Session) Get(route string, headers http.Header) Response {
+	return ar.makeRequest(http.MethodGet, route, headers, nil)
 }
 
-func (ar *AppRequest[T]) PostForm(route string, headers http.Header, formValues url.Values) {
+func (ar *Session) PostForm(route string, headers http.Header, formValues url.Values) Response {
 	if headers == nil {
 		headers = make(http.Header)
 	}
@@ -50,43 +47,14 @@ func (ar *AppRequest[T]) PostForm(route string, headers http.Header, formValues 
 	headers.Set("Content-Type", "application/x-www-form-urlencoded")
 
 	data := strings.NewReader(formValues.Encode())
-	ar.makeRequest(http.MethodPost, route, headers, data)
+	return ar.makeRequest(http.MethodPost, route, headers, data)
 }
 
-func (ar *AppRequest[T]) IsRedirect() bool {
-	if ar.LastResponse == nil {
-		return false
-	}
-
-	for _, redirectCode := range RedirectCodes {
-		if ar.LastResponse.Code == redirectCode {
-			return true
-		}
-	}
-
-	return false
+func (ar *Session) FollowRedirect(res Response) Response {
+	return ar.makeRequest(http.MethodGet, res.Header().Get("Location"), nil, nil)
 }
 
-func (ar *AppRequest[T]) IsOK() bool {
-	if ar.LastResponse == nil {
-		return false
-	}
-
-	return ar.LastResponse.Code == 200
-}
-
-func (ar *AppRequest[T]) Code() int {
-	return ar.LastResponse.Code
-}
-
-func (ar *AppRequest[T]) FollowRedirect() {
-	ar.makeRequest(http.MethodGet, ar.LastResponse.Header().Get("Location"), nil, nil)
-}
-
-func (ar *AppRequest[T]) Body() string {
-	return ar.LastResponse.Body.String()
-}
-func (ar *AppRequest[T]) makeRequest(method string, route string, headers http.Header, data io.Reader) {
+func (ar *Session) makeRequest(method string, route string, headers http.Header, data io.Reader) Response {
 	req := httptest.NewRequest(method, route, data)
 	req.URL.Scheme = "http"
 	req.URL.Host = "app.test"
@@ -103,8 +71,41 @@ func (ar *AppRequest[T]) makeRequest(method string, route string, headers http.H
 		req.AddCookie(cookie)
 	}
 
-	ar.LastResponse = httptest.NewRecorder()
+	recorder := httptest.NewRecorder()
 
-	ar.medium.ServeHTTP(ar.LastResponse, req)
-	ar.CookieJar.SetCookies(req.URL, ar.LastResponse.Result().Cookies())
+	ar.medium.ServeHTTP(recorder, req)
+	ar.CookieJar.SetCookies(req.URL, recorder.Result().Cookies())
+
+	return Response{RawResponse: recorder}
+}
+
+type Response struct {
+	RawResponse *httptest.ResponseRecorder
+}
+
+func (r *Response) Code() int {
+	return r.RawResponse.Code
+}
+
+func (r *Response) IsRedirect() bool {
+	for _, redirectCode := range RedirectCodes {
+		if r.Code() == redirectCode {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (r *Response) IsOK() bool {
+	return r.Code() == http.StatusOK
+}
+
+func (r *Response) Body() string {
+	body, _ := io.ReadAll(r.RawResponse.Result().Body)
+	return string(body)
+}
+
+func (r *Response) Header() http.Header {
+	return r.RawResponse.Result().Header
 }
